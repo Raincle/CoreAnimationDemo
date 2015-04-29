4190db510e6432a28c0f70795f6a4a7ec574bfee
Raincle <yaweihong@icloud.com>
2015-04-29 11:40:13 +0800

    block

--- CoreAnimationDemo/CoreAnimationDemo/ViewController.swift
+++ CoreAnimationDemo/CoreAnimationDemo/ViewController.swift
@@ -8,10 +8,37 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var block: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+
+        block.layer.cornerRadius = 10
+        block.layer.shadowOffset = CGSizeMake(2, 2)
+        block.layer.shadowOpacity = 0.8
+        block.layer.shadowRadius = 3
+        block.layer.shadowColor = UIColor.greenColor().CGColor
+        
+        let animationR = CABasicAnimation(keyPath: "cornerRadius")
+        animationR.fromValue = block.layer.cornerRadius
+        animationR.toValue = 25
+        animationR.autoreverses = true
+        animationR.repeatCount = HUGE
+        animationR.duration = 1
+        block.layer.addAnimation(animationR, forKey: "cornerRadius")
+        
+        let animationS = CABasicAnimation(keyPath: "shadowRadius")
+        animationS.fromValue = block.layer.shadowRadius
+        animationS.toValue = 100
+        animationS.autoreverses = true
+        animationS.repeatCount = HUGE
+        animationS.duration = 1
+        block.layer.addAnimation(animationS, forKey: "shadowRadius")
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
